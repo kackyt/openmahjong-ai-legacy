0473a3fe0ff45f6f2125b3ccd8de0af77cf4429b
kacky <kacky@7d002cef-7586-754a-a61e-837dff7ef8f4>
2011-10-06 13:21:39 +0000

    OpenMahjongClient: <新規追加> ・r154の機能追加で必要なファイルを登録しました。
    git-svn-id: svn+ssh://svn.sourceforge.jp/svnroot/openmahjong/common/trunk@155 7d002cef-7586-754a-a61e-837dff7ef8f4

---
+++ XMLLib/TakuListener.cpp
@@ -0,0 +1,40 @@
+#include "TakuListener.h"
+
+OMTakuListener::OMTakuListener()
+{
+}
+
+void OMTakuListener::tehaiAdded(OMTaku *taku, int memberIndex, OMMember *member, int paiIndex, OMPai *pai)
+{
+
+}
+
+void OMTakuListener::tehaiRemoved(OMTaku *taku, int memberIndex, OMMember *member, int paiIndex, OMPai *pai)
+{
+
+}
+
+void OMTakuListener::dahaiAdded(OMTaku *taku, int memberIndex, OMMember *member, int paiIndex, OMPai *pai)
+{
+
+}
+
+void OMTakuListener::dahaiRemoved(OMTaku *taku, int memberIndex, OMMember *member, int paiIndex, OMPai *pai)
+{
+
+}
+
+void OMTakuListener::dahaiNaki(OMTaku *taku, int memberIndex, OMMember *member, int paiIndex, OMPai *pai)
+{
+
+}
+
+void OMTakuListener::nakiAdded(OMTaku *taku, int memberIndex, OMMember *member, OMNakiMentsu *mentsu)
+{
+
+}
+
+void OMTakuListener::nakiRemoved(OMTaku *taku, int memberIndex, OMMember *member, OMNakiMentsu *mentsu)
+{
+
+}
---
+++ XMLLib/TakuListener.h
@@ -0,0 +1,21 @@
+#ifndef TAKULISTENER_H
+#define TAKULISTENER_H
+#include "Taku.h"
+#include "Member.h"
+#include "Pai.h"
+#include "NakiMentsu.h"
+
+class OMTakuListener
+{
+public:
+    OMTakuListener();
+    virtual void tehaiAdded(OMTaku *taku,int memberIndex,OMMember *member,int paiIndex,OMPai *pai);
+    virtual void tehaiRemoved(OMTaku *taku,int memberIndex,OMMember *member,int paiIndex,OMPai *pai);
+    virtual void dahaiAdded(OMTaku *taku,int memberIndex,OMMember *member,int paiIndex,OMPai *pai);
+    virtual void dahaiRemoved(OMTaku *taku,int memberIndex,OMMember *member,int paiIndex,OMPai *pai);
+    virtual void dahaiNaki(OMTaku *taku,int memberIndex,OMMember *member,int paiIndex,OMPai *pai);
+    virtual void nakiAdded(OMTaku *taku,int memberIndex,OMMember *member,OMNakiMentsu *mentsu);
+    virtual void nakiRemoved(OMTaku *taku,int memberIndex,OMMember *member,OMNakiMentsu *mentsu);
+};
+
+#endif // TAKULISTENER_H
